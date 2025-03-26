49b856aabe9d0d4bdca3954d1c902984e8dd7e95
Eliiiise <balvalloire@gmail.com>
2025-03-26 17:39:04 +0100

    ui: layout dialog reveal

--- components/RevealDialog.tsx
+++ components/RevealDialog.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { Dialog, DialogContent } from "./ui/dialog";
 import { Button } from "./ui/button";
 import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 type NFT = {
   id: number;
@@ -35,24 +37,60 @@ export default function RevealDialog({
 }: RevealDialogProps) {
   // Check if there's only one NFT
   const isSingleNFT = revealedNFTs.length === 1;
+  const [fadeClass, setFadeClass] = useState("");
+
+  const handleNavigate = (direction: "prev" | "next") => {
+    setFadeClass("slider-fade-out");
+  
+    setTimeout(() => {
+      if (direction === "prev") {
+        navigateToPrev();
+      } else {
+        navigateToNext();
+      }
+      setFadeClass("slider-fade-in");
+    }, 600);
+  };
+
+  const rarityStyles: Record<string, string> = {
+    common: "bg-[#98FCE4] text-black",
+    rare: "bg-[#6CC50C] text-white",
+    legendary: "bg-gradient-to-r from-[#FCC400] to-[#CC7C09] text-black",
+    mythic: "bg-gradient-to-r from-[#E4205E] to-[#E420AB] text-black",
+  };
+
+  const rarityColors: Record<string, string> = {
+    common: "#98FCE4",
+    rare: "#6CC50C",
+    legendary: "#FFB323",
+    mythic: "#E4205B",
+  };
+
+  const rarityAttribute = currentNFT.attributes.find(
+    (attr) => attr.trait_type.toLowerCase() === "rarity"
+  );
+  const rarityValue = rarityAttribute ? rarityAttribute.value.toLowerCase() : "common";
+  const rarityClass = rarityStyles[rarityValue] || rarityStyles["common"];
+  const rarityColor = rarityColors[rarityValue] || rarityColors["common"];
+
 
   return (
     <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
       <DialogContent
         title="reveal hydros"
-        className="backdrop-blur-md !border-none !bg-transparent justify-center max-w-[95vw] mx-auto sm:max-w-[90vw] md:max-w-[85vw] lg:max-w-[1100px] p-3 sm:p-4 md:p-6"
+        className="!border-none !bg-transparent !shadow-none justify-around h-[85vh] !max-w-[100vw] w-[96vw] lg:w-[80vw] inset-0 translate-x-0 translate-y-0 m-auto p-0"
       >
-        <div className="flex flex-col h-full w-full justify-center">
-          <div className="flex-1 flex items-center justify-center">
+        <div className="flex flex-col h-full w-[96vw] lg:w-[80vw] justify-around mt-[3vh]">
+          <div className="flex-1 flex items-center justify-around">
             {/* Carousel implementation */}
             {revealedNFTs.length > 0 && (
-              <div className="relative w-full flex flex-col gap-4">
+              <div className={`relative w-full flex-1 h-full justify-around flex flex-col gap-4`}>
                 {/* Carousel Component */}
-                <div className="flex flex-col md:flex-row items-center justify-center w-full gap-4">
+                <div className="flex items-center justify-center w-full gap-4">
                   {/* Previous button - only show if there are multiple NFTs */}
                   {!isSingleNFT && (
                     <Button
-                      onClick={navigateToPrev}
+                      onClick={() => handleNavigate("prev")}
                       disabled={!canNavigatePrev()}
                       className={`flex-shrink-0 w-13 h-10 rounded-full flex items-center justify-center order-1 
                       ${
@@ -67,39 +105,48 @@ export default function RevealDialog({
                   )}
 
                   <div
-                    className={`flex flex-col md:flex-row items-center ${
-                      isSingleNFT ? "w-full" : "w-full space-x-10"
-                    } order-2`}
+                    className={`flex flex-col md:flex-row justify-center items-center ${
+                      isSingleNFT ? "w-full" : "w-full md:gap-10 gap-[4vh]"
+                    } order-2 slider ${fadeClass}`}
                   >
                     {/* NFT Image - Left Side (Top on mobile) */}
-                    <div className="shadow-[0_0_100px_rgba(45,212,191,0.7)] w-full max-w-[350px] h-[350px] sm:w-[400px] sm:h-[400px] md:w-[350px] md:h-[350px] lg:w-[480px] lg:h-[480px] relative bg-gray-800/40 rounded-lg overflow-hidden order-1">
+                    <div className="aspect-square w-full max-w-[30vh] md:max-w-[65vh] relative  order-1">
+                      <motion.div
+                              initial={{ backgroundColor: `${rarityColor}00` }}
+                              animate={{ backgroundColor: `${rarityColor}44` }}
+                              transition={{ duration: 0.6, ease: "easeInOut", delay: 0.2 }}
+                              className="absolute w-full h-full rounded-2xl blur-3xl overflow-hidden scale-[1.15]"
+                            >
+                      </motion.div>
                       {currentNFT && (
                         <Image
                           src={currentNFT.image}
                           alt={currentNFT.name}
                           fill
-                          className="object-cover"
+                          className="image-slider object-cover rounded-lg"
                         />
                       )}
                     </div>
 
                     {/* NFT Details - Right Side (Bottom on mobile) */}
-                    <div className="w-full max-w-[350px] h-[350px] sm:w-[400px] sm:h-[400px] md:w-[350px] md:h-[350px] lg:w-[480px] lg:h-[480px] bg-[#FAFAFA0D] rounded-lg p-4 md:p-6 lg:p-8 flex flex-col mt-6 md:mt-0 order-2">
+                    <div className="content-slider md:aspect-square w-full max-w-[80vw] md:max-w-[65vh] bg-[#FAFAFA0D] rounded-lg flex flex-col justify-center order-2">
                       {currentNFT && (
-                        <div className="flex flex-col text-center">
-                          <div className="text-gray-400 text-xs sm:text-sm mb-1 sm:mb-2">
-                            HYDROS #{currentNFT.id}
+                        <div className="flex flex-col text-center max-w-[400px] w-full mx-auto lg:gap-[5vh] md:gap-[4vh] gap-[3vh] px-[2vw] p-4 md:p-6 lg:p-8">
+                          <div>
+                            <p className="text-gray-400 text-xs sm:text-sm font-herculanum">
+                              HYDROS #{currentNFT.id}
+                            </p>
+                            <h2 className="text-white text-xl sm:text-2xl md:text-3xl font-herculanum">
+                              {currentNFT.name}
+                            </h2>
                           </div>
-                          <h2 className="text-white text-xl sm:text-2xl md:text-3xl font-herculanum mb-2 sm:mb-4 md:mb-6">
-                            {currentNFT.name}
-                          </h2>
 
                           {/* Rarity Badge */}
                           {currentNFT.attributes.find(
                             (attr) => attr.trait_type.toLowerCase() === "rarity"
                           ) && (
-                            <div className="mb-2 sm:mb-4 md:mb-6">
-                              <span className="inline-block px-2 py-1 sm:px-3 md:px-4 md:py-1 rounded-full bg-pink-600 text-white text-xs md:text-sm font-medium uppercase">
+                            <div className="mb-[3vh]">
+                              <span className={`inline-block px-2 py-1 sm:px-3 md:px-4 md:py-1 rounded-full font-herculanum text-xs md:text-sm font-medium uppercase ${rarityClass}`}>
                                 {
                                   currentNFT.attributes.find(
                                     (attr) =>
@@ -112,16 +159,16 @@ export default function RevealDialog({
 
                           {/* Attributes List */}
                           <div className="flex-1 overflow-auto">
-                            <div className="space-y-2 sm:space-y-3 md:space-y-4">
+                            <div className="">
                               {currentNFT.attributes.map((attr, index) => (
                                 <div
                                   key={index}
                                   className="flex justify-between"
                                 >
-                                  <span className="text-gray-400 uppercase font-herculanum  md:text-base">
+                                  <span className="text-gray-400 w-2/5 text-left uppercase font-herculanum  md:text-base">
                                     {attr.trait_type}
                                   </span>
-                                  <span className="text-teal-400 uppercase font-herculanum  md:text-base">
+                                  <span className="uppercase flex-1 text-left font-herculanum  md:text-base" style={{color: `${rarityColor}`}}>
                                     {attr.value}
                                   </span>
                                 </div>
@@ -136,7 +183,7 @@ export default function RevealDialog({
                   {/* Next button - only show if there are multiple NFTs */}
                   {!isSingleNFT && (
                     <Button
-                      onClick={navigateToNext}
+                      onClick={() => handleNavigate("next")}
                       disabled={!canNavigateNext()}
                       className={`flex-shrink-0 w-10 h-10 rounded-full flex items-center justify-center order-3 
                       ${
